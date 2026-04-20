fb09d75b50632917d983ed6164653be4d494517a
bwolfkill <bwolfkill29@gmail.com>
2026-04-20 19:56:38 -0400

    Remove nullable from mapped_columns

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -7,5 +7,5 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4, unique=True, index=True)
-    created_at: Mapped[datetime] = mapped_column(default=func.now(), nullable=False)
-    updated_at: Mapped[datetime] = mapped_column(default=func.now(), onupdate=func.now(), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(default=func.now(), onupdate=func.now())
